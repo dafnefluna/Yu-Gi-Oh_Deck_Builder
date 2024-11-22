d295f8700cbaf7481d47204c8976a893c3eb127a
andrew-surrena <andrewsurrena@gmail.com>
2024-11-21 22:43:51 -0600

    deckpage edits

--- client/src/pages/DeckPage.tsx
+++ client/src/pages/DeckPage.tsx
@@ -1,61 +1,65 @@
-import Accordion from 'react-bootstrap/Accordion';
-import { Col, Card } from 'react-bootstrap';
-import { Decks } from '../interfaces/Deck.js'
+import { Container } from 'react-bootstrap';
+import { Pagination } from 'antd';
+import { Decks } from '../interfaces/Deck';
 import React, { useEffect, useState } from 'react';
 import { useQuery } from '@apollo/client';
-import { QUERY_GETALLDECKS } from '../utils/queries.js';
-// import CardtoDeck from '../components/CardToDeck.js';
-
-
+import { QUERY_GETALLDECKS } from '../utils/queries';
+import DeckList from '../components/DeckLayout';
 
 const DeckPage: React.FC = () => {
+    const [decks, setDecks] = useState<Decks[]>([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [currentCardsPer, setCardsPer] = useState(6);
 
-    // establisha  state for the card data
-    const [decks, setDecks] = useState<Decks[] | null>([]);
-    // establish the usequery for get all cards
-
-// do i need variables if im getting all cards should it just be an open  object
     const { data: deckDataQuery, loading: deckLoading, error: deckError } = useQuery(QUERY_GETALLDECKS);
-    
-        useEffect(() => {
-            if (deckDataQuery) {
-                console.timeLog(deckDataQuery)
-                setDecks(deckDataQuery.allDecks);
-            }
-        }, [deckDataQuery]);
 
-        if (deckLoading ) return <p>Loading...</p>;
-        if (deckError) return <p>Error loading deck data: {deckError.message}</p>;
+    useEffect(() => {
+        if (deckDataQuery) {
+            console.log('Deck Data Query:', deckDataQuery);
+            setDecks(deckDataQuery?.allDecks || []);
+        }
+    }, [deckDataQuery]);
 
+    useEffect(() => {
+        setCurrentPage(1);
+    }, []);
 
+    if (deckLoading) return <p>Loading...</p>;
+    if (deckError) return <p>Error loading deck data: {deckError.message}</p>;
+
+    const lastIndex = currentPage * currentCardsPer;
+    const firstIndex = lastIndex - currentCardsPer;
+    const currentDecks = decks.slice(firstIndex, lastIndex);
+
+    const onPageChange = (page: number, size: number) => {
+        setCurrentPage(page);
+        setCardsPer(size);
+    };
 
     return (
-        <Col md="4" border='dark'>
-        <Accordion >
-            {decks?.map((deck, index) =>(
-            <Accordion.Item eventKey={index.toString()} key={deck.id}>
-                <Accordion.Header>
-                {deck.cards[0].image ? (
-                            <Card.Img src={deck.cards[0].image} alt={`Art for ${deck.cards[0].name}`} variant='top' />
-                        ) : null}
-                </Accordion.Header>
-                <Accordion.Body>
-                    <Card border='dark'>
-                    <Card.Body>
-                            <Card.Text>{deck.name}</Card.Text>
-                            <Card.Text>Playable?: {deck.playable}</Card.Text>
-                            <Card.Text>Type: {deck.type}</Card.Text>
-                            <Card.Text>Saved Cards</Card.Text>
-                    </Card.Body>
-                    </Card>
+        <Container>
+            <h2 className="pt-5">
+                {decks.length
+                    ? `You have ${decks.length} decks:`
+                    : `Please create a deck first!`}
+            </h2>
 
-                </Accordion.Body>
-            </Accordion.Item>
-                ))}
-        </Accordion>
-        </Col>
+            <DeckList decks={currentDecks} />
+
+            <div className="d-flex justify-content-center mt-4 mb-4 pagination-container">
+                <Pagination
+                    hideOnSinglePage={true}
+                    pageSize={currentCardsPer}
+                    total={decks.length} // Total items, not pages
+                    current={currentPage}
+                    showSizeChanger={true}
+                    pageSizeOptions={['6', '12', '18', '24']}
+                    onChange={onPageChange}
+                    size={'default'}
+                />
+            </div>
+        </Container>
     );
 };
-
 
 export default DeckPage;
