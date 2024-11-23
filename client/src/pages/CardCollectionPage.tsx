2447493e21d77a8dcd55a3bae90de3e69e9455bb
“[firstname <dafne.faviola.luna@gmail.com>
2024-11-22 20:26:07 -0500

    added google font throughout app

--- client/src/pages/CardCollectionPage.tsx
+++ client/src/pages/CardCollectionPage.tsx
@@ -38,113 +38,113 @@ const CardCollectionPage: React.FC = () => {
 
     if (!isLoggedIn) {
         return (
-          <div className="containerPage">
-            <Card
-              style={{
-                width: 400,
-                textAlign: "center",
-                boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
-                backgroundColor: "#f0f2f5",
-              }}
-            ><h2 style={{color: "yellow", background: "black", borderRadius: "10px",}}>Whoah! You've gone BANANAS! Please log in before viewing saved cards 🍌</h2>
-              <iframe
-                src="https://giphy.com/embed/H8uuXYln5pxVVLFn7k"
-                height="270"
-                frameBorder="0"
-                allowFullScreen
-                title="Bananas GIF"
-                style={{ marginBottom: "16px", width: "100%" }}
-              ></iframe>
-              <Link to="/">
-                <Button type="primary">Go to Login Page</Button>
-              </Link>
-            </Card>
-          </div>
+            <div className="containerPage">
+                <Card
+                    style={{
+                        width: 400,
+                        textAlign: "center",
+                        boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
+                        backgroundColor: "#f0f2f5",
+                    }}
+                ><h2 style={{ color: "yellow", background: "black", borderRadius: "10px", }}>Whoah! You've gone BANANAS! Please log in before viewing saved cards 🍌</h2>
+                    <iframe
+                        src="https://giphy.com/embed/H8uuXYln5pxVVLFn7k"
+                        height="270"
+                        frameBorder="0"
+                        allowFullScreen
+                        title="Bananas GIF"
+                        style={{ marginBottom: "16px", width: "100%" }}
+                    ></iframe>
+                    <Link to="/">
+                        <Button type="primary">Go to Login Page</Button>
+                    </Link>
+                </Card>
+            </div>
         );
-      }  
+    }
 
     return (
         <>
             <div className='backgroundStyle'></div>
             <Container className="text-center my-4">
-            <h2>My Yu-Gi-Oh Collection</h2>
+                <h1 id='collectiontitle'>My Yu-Gi-Oh Collection</h1>
                 <Row>
-                <Col xs={12} md={4} class='accordion'>
-                    <Accordion >
-                        {group1.map((card, index) => (
-                            <Accordion.Item eventKey={index.toString()} key={card.id}>
-                                <Accordion.Header>
-                                    {card.image ? (
-                                        <Card.Img src={card.image} alt={`Art for ${card.name}`} variant='top' />
-                                    ) : null}
-                                </Accordion.Header>
-                                <Accordion.Body>
-                                    <Card border='dark'>
-                                        <Card.Body>
-                                            <Card.Text>{card.name}</Card.Text>
-                                            <Card.Text>ATR: {card.attribute} | Level: {card.level}</Card.Text>
-                                            <Card.Text>[{card.race} / {card.type}]</Card.Text>
-                                            <Card.Text>{card.description}</Card.Text>
-                                            <Card.Text>ATK: {card.attack} | DEF: {card.defense}</Card.Text>
-                                            <><CardtoDeck card={card} /></>
-                                        </Card.Body>
-                                    </Card>
-                                </Accordion.Body>
-                            </Accordion.Item>
-                        ))}
-                    </Accordion>
-                </Col>
-                <Col xs={12} md={4} >
-                    <Accordion>
-                        {group2.map((card, index) => (
-                            <Accordion.Item eventKey={index.toString()} key={card.id}>
-                                <Accordion.Header>
-                                    {card.image ? (
-                                        <Card.Img src={card.image} alt={`Art for ${card.name}`} variant='top' />
-                                    ) : null}
-                                </Accordion.Header>
-                                <Accordion.Body>
-                                    <Card border='dark'>
-                                        <Card.Body>
-                                            <Card.Text>{card.name}</Card.Text>
-                                            <Card.Text>ATR: {card.attribute} | Level: {card.level}</Card.Text>
-                                            <Card.Text>[{card.race} / {card.type}]</Card.Text>
-                                            <Card.Text>{card.description}</Card.Text>
-                                            <Card.Text>ATK: {card.attack} | DEF: {card.defense}</Card.Text>
-                                            <><CardtoDeck card={card} /></>
-                                        </Card.Body>
-                                    </Card>
-                                </Accordion.Body>
-                            </Accordion.Item>
-                        ))}
-                    </Accordion>
-                </Col>
-                <Col xs={12} md={4}>
-                    <Accordion>
-                        {group3.map((card, index) => (
-                            <Accordion.Item eventKey={index.toString()} key={card.id}>
-                                <Accordion.Header>
-                                    {card.image ? (
-                                        <Card.Img src={card.image} alt={`Art for ${card.name}`} variant='top' />
-                                    ) : null}
-                                </Accordion.Header>
-                                <Accordion.Body>
-                                    <Card border='dark'>
-                                        <Card.Body>
-                                            <Card.Text>{card.name}</Card.Text>
-                                            <Card.Text>ATR: {card.attribute} | Level: {card.level}</Card.Text>
-                                            <Card.Text>[{card.race} / {card.type}]</Card.Text>
-                                            <Card.Text>{card.description}</Card.Text>
-                                            <Card.Text>ATK: {card.attack} | DEF: {card.defense}</Card.Text>
-                                            <><CardtoDeck card={card} /></>
-                                        </Card.Body>
-                                    </Card>
-                                </Accordion.Body>
-                            </Accordion.Item>
-                        ))}
-                    </Accordion>
-                </Col>
-            </Row>
+                    <Col xs={12} md={4} class='accordion'>
+                        <Accordion >
+                            {group1.map((card, index) => (
+                                <Accordion.Item eventKey={index.toString()} key={card.id}>
+                                    <Accordion.Header>
+                                        {card.image ? (
+                                            <Card.Img src={card.image} alt={`Art for ${card.name}`} variant='top' />
+                                        ) : null}
+                                    </Accordion.Header>
+                                    <Accordion.Body>
+                                        <Card border='dark'>
+                                            <Card.Body>
+                                                <Card.Text>{card.name}</Card.Text>
+                                                <Card.Text>ATR: {card.attribute} | Level: {card.level}</Card.Text>
+                                                <Card.Text>[{card.race} / {card.type}]</Card.Text>
+                                                <Card.Text>{card.description}</Card.Text>
+                                                <Card.Text>ATK: {card.attack} | DEF: {card.defense}</Card.Text>
+                                                <><CardtoDeck card={card} /></>
+                                            </Card.Body>
+                                        </Card>
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                            ))}
+                        </Accordion>
+                    </Col>
+                    <Col xs={12} md={4} >
+                        <Accordion>
+                            {group2.map((card, index) => (
+                                <Accordion.Item eventKey={index.toString()} key={card.id}>
+                                    <Accordion.Header>
+                                        {card.image ? (
+                                            <Card.Img src={card.image} alt={`Art for ${card.name}`} variant='top' />
+                                        ) : null}
+                                    </Accordion.Header>
+                                    <Accordion.Body>
+                                        <Card border='dark'>
+                                            <Card.Body>
+                                                <Card.Text>{card.name}</Card.Text>
+                                                <Card.Text>ATR: {card.attribute} | Level: {card.level}</Card.Text>
+                                                <Card.Text>[{card.race} / {card.type}]</Card.Text>
+                                                <Card.Text>{card.description}</Card.Text>
+                                                <Card.Text>ATK: {card.attack} | DEF: {card.defense}</Card.Text>
+                                                <><CardtoDeck card={card} /></>
+                                            </Card.Body>
+                                        </Card>
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                            ))}
+                        </Accordion>
+                    </Col>
+                    <Col xs={12} md={4}>
+                        <Accordion>
+                            {group3.map((card, index) => (
+                                <Accordion.Item eventKey={index.toString()} key={card.id}>
+                                    <Accordion.Header>
+                                        {card.image ? (
+                                            <Card.Img src={card.image} alt={`Art for ${card.name}`} variant='top' />
+                                        ) : null}
+                                    </Accordion.Header>
+                                    <Accordion.Body>
+                                        <Card border='dark'>
+                                            <Card.Body>
+                                                <Card.Text>{card.name}</Card.Text>
+                                                <Card.Text>ATR: {card.attribute} | Level: {card.level}</Card.Text>
+                                                <Card.Text>[{card.race} / {card.type}]</Card.Text>
+                                                <Card.Text>{card.description}</Card.Text>
+                                                <Card.Text>ATK: {card.attack} | DEF: {card.defense}</Card.Text>
+                                                <><CardtoDeck card={card} /></>
+                                            </Card.Body>
+                                        </Card>
+                                    </Accordion.Body>
+                                </Accordion.Item>
+                            ))}
+                        </Accordion>
+                    </Col>
+                </Row>
             </Container>
 
         </>
